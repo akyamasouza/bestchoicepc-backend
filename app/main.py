7a0c3d450068f1ca592b2c4ef3c1c69ba2038914
akyama <akyama.souza@gmail.com>
2026-04-01 14:07:09 -0400

    fix(api): adicionar config de CORS para chamadas externas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.core.database import close_mongo_client
@@ -21,6 +22,15 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(title=settings.app_name, lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(cpus_router)
 app.include_router(gpus_router)
 app.include_router(ssds_router)
